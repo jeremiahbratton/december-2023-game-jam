344c15f40c24dda3efaf148b8c02de641dc94ddd
Jeremiah Bratton <>
2023-12-15 13:39:36 -0800

    FRUITS!

--- src/PortentGameScene.js
+++ src/PortentGameScene.js
@@ -6,27 +6,29 @@ export default class PortentGame extends Phaser.Scene {
 	}
 
 	preload() {
-		this.load.setBaseURL('https://labs.phaser.io')
+		//this.load.setBaseURL('https://labs.phaser.io')
 
-		this.load.image('sky', 'assets/skies/space3.png')
-		this.load.image('blueberry', 'assets/particles/blue.png')
-		this.load.image('cherry', 'assets/particles/red.png')
-		this.load.image('strawberry', 'assets/particles/red.png')
-		this.load.image('lemon', 'assets/particles/yellow.png')
-		this.load.image('orange', 'assets/particles/red.png')
-		this.load.image('apple', 'assets/particles/red.png')
-		this.load.image('pear', 'assets/particles/yellow.png')
-		this.load.image('peach', 'assets/particles/red.png')
-		this.load.image('pineapple', 'assets/particles/green.png')
-		this.load.image('melon', 'assets/particles/green.png')
-		this.load.image('watermelon', 'assets/particles/green.png')
-		}
+		this.load.image('sky', 'https://labs.phaser.io/assets/skies/space3.png')
+		//this.load.image('cherry', 'assets/particles/red.png')
 
-		/**
-		 * Create a game where we drop balls of a random size into a bucket.
-		 * 
-		 * When balls collide with each other, if they are the same size, they merge.
-		 */
+		this.load.image('blueberry', 'img/blueberry.png')
+		this.load.image('cherry', 'img/cherry.png')
+		this.load.image('strawberry', 'img/strawberry.png')
+		this.load.image('lemon', 'img/lemon.png')
+		this.load.image('orange', 'img/orange.png')
+		this.load.image('apple', 'https://labs.phaser.io/assets/particles/red.png')
+		this.load.image('pear', 'https://labs.phaser.io/assets/particles/yellow.png')
+		this.load.image('peach', 'https://labs.phaser.io/assets/particles/red.png')
+		this.load.image('pineapple', 'https://labs.phaser.io/assets/particles/green.png')
+		this.load.image('melon', 'https://labs.phaser.io/assets/particles/green.png')
+		this.load.image('watermelon', 'https://labs.phaser.io/assets/particles/green.png')
+	}
+
+	/**
+	 * Create a game where we drop balls of a random size into a bucket.
+	 * 
+	 * When balls collide with each other, if they are the same size, they merge.
+	 */
 	create() {
 		/**
 		 * Set world bounds for game.
@@ -39,35 +41,19 @@ export default class PortentGame extends Phaser.Scene {
 
 		const fruits = [
 			{'name':'blueberry', 'value':2, 'width': 50, 'height': 50},
-			{'name':'cherry', 'value':4},
-			{'name':'strawberry', 'value':8},
-			{'name':'lemon', 'value':16},
-			{'name':'orange', 'value':24},
-			{'name':'apple', 'value':48},
-			{'name':'pear', 'value':96},
-			{'name':'peach', 'value':192},
-			{'name':'pineapple', 'value':384},
-			{'name':'melon', 'value':768},
-			{'name':'watermelon', 'value':1536, 'width': 256, 'height': 256},
+			{'name':'cherry', 'value':4, 'width': 60, 'height': 60},
+			{'name':'strawberry', 'value':8, 'width': 71, 'height': 71},
+			{'name':'lemon', 'value':16, 'width': 86, 'height': 86},
+			{'name':'orange', 'value':24, 'width': 103, 'height': 103},
+			{'name':'apple', 'value':48, 'width': 123, 'height': 123},
+			{'name':'pear', 'value':96, 'width': 148, 'height': 148},
+			{'name':'pineapple', 'value':192, 'width': 178, 'height': 178},
+			{'name':'melon', 'value':384, 'width': 213, 'height': 213},
+			{'name':'watermelon', 'value':768, 'width': 256, 'height': 256},
+			//{'name':'watermelon', 'value':1536, 'width': 256, 'height': 256},
+			//{'name':'peach', 'value':192, 'width': 256, 'height': 256},
 		];
 
-		// const getFruit = () => {
-		// 	const fruits = [
-		// 		{'name':'blueberry', 'value':2},
-		// 		{'name':'cherry', 'value':4},
-		// 		{'name':'strawberry', 'value':8},
-		// 		{'name':'lemon', 'value':16},
-		// 		{'name':'orange', 'value':24},
-		// 		{'name':'apple', 'value':48},
-		// 		{'name':'pear', 'value':96},
-		// 		{'name':'peach', 'value':192},
-		// 		{'name':'pineapple', 'value':384},
-		// 		{'name':'melon', 'value':768},
-		// 		{'name':'watermelon', 'value':1536, 'width': 256, 'height': 256},
-		// 	];
-		// 	return fruits;
-		// }
-		
 		const createFruit = () => {
 			//const fruits = getFruit();
 			// Generate a random index
@@ -107,29 +93,44 @@ export default class PortentGame extends Phaser.Scene {
 			console.log(activefruits);
 		});
 
+		const matterWorld = this.matter.world; // Capture the matter.world in a variable
+
 		/**
 		 * Collision event handler
 		 */
 		this.matter.world.on('collisionstart', function (event, bodyA, bodyB) {
-			if(bodyA.label === "fruit" && bodyB.label === "fruit"){
-				console.log(event);
-				console.log("fruitA", bodyA.gameObject.texture.key);
-				console.log("fruitB", bodyB.gameObject.texture.key);
+			if (bodyA.label === "fruit" && bodyB.label === "fruit") {
+				//console.log(event);
+				//console.log("fruitA", bodyA.gameObject.texture.key);
+				//console.log("fruitB", bodyB.gameObject.texture.key);
 				if(bodyA.gameObject.texture.key === bodyB.gameObject.texture.key) {
 					console.log(`MATCH ${bodyA.gameObject.texture.key} and ${bodyB.gameObject.texture.key}`);
 					//this.matter.remove(event.world, bodyB);
 					
 					// Lookup our upgraded fruit based on value. 2x the current fruit.
-					const upgradedFruit = fruits.find(item => item.value === (bodyA.gameObject.value*2));
+					console.log('bodyA value:', [bodyA.gameObject.texture.key, bodyA.gameObject.value]);
+					console.log('finding fruit with value of', (bodyA.gameObject.value*2));
 
-					// Check if the new texture is already loaded
-					if (!this.textures.exists(upgradedFruit.name)) {
-						// If not, dynamically load the new texture
-						this.load.image(upgradedFruit.name);
+					// If two watermelons collide, delete/remove both
+					if (bodyA.gameObject.texture.key == 'watermelon') {
+						 // Destroy both game objects
+						bodyA.destroy();
+						bodyB.destroy();
+						// Remove the bodies from the physics world
+						matterWorld.remove(bodyA);
+						matterWorld.remove(bodyB);
+					} else {
+						const upgradedFruit = fruits.find(item => item.value === (bodyA.gameObject.value*2));
+						console.log('upgradedFruit', upgradedFruit);
+
+						// Set the new texture for the game object
+						bodyA.gameObject.setTexture(upgradedFruit.name);
+
+						// Destroy bodyB
+						bodyB.destroy();
+						matterWorld.remove(bodyB);
 					}
-
-					// Set the new texture for the game object
-					bodyA.gameObject.setTexture(upgradedFruit.name);
+					
 				}
 			}
 		});
