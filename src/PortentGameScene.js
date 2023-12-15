a09a5ed21efddd86babe73b0526d3e3b5c19b601
Jeremiah Bratton <>
2023-12-15 13:05:51 -0800

    Updates

--- src/PortentGameScene.js
+++ src/PortentGameScene.js
@@ -36,21 +36,40 @@ export default class PortentGame extends Phaser.Scene {
 		let activefruits = [];
 
 		this.matter.world.setBounds(0, 0, bucketWidth, bucketHeight, 32, true, true, false, true);
+
+		const fruits = [
+			{'name':'blueberry', 'value':2, 'width': 50, 'height': 50},
+			{'name':'cherry', 'value':4},
+			{'name':'strawberry', 'value':8},
+			{'name':'lemon', 'value':16},
+			{'name':'orange', 'value':24},
+			{'name':'apple', 'value':48},
+			{'name':'pear', 'value':96},
+			{'name':'peach', 'value':192},
+			{'name':'pineapple', 'value':384},
+			{'name':'melon', 'value':768},
+			{'name':'watermelon', 'value':1536, 'width': 256, 'height': 256},
+		];
+
+		// const getFruit = () => {
+		// 	const fruits = [
+		// 		{'name':'blueberry', 'value':2},
+		// 		{'name':'cherry', 'value':4},
+		// 		{'name':'strawberry', 'value':8},
+		// 		{'name':'lemon', 'value':16},
+		// 		{'name':'orange', 'value':24},
+		// 		{'name':'apple', 'value':48},
+		// 		{'name':'pear', 'value':96},
+		// 		{'name':'peach', 'value':192},
+		// 		{'name':'pineapple', 'value':384},
+		// 		{'name':'melon', 'value':768},
+		// 		{'name':'watermelon', 'value':1536, 'width': 256, 'height': 256},
+		// 	];
+		// 	return fruits;
+		// }
 		
 		const createFruit = () => {
-			const fruits = [
-				{'name':'blueberry', 'value':2},
-				{'name':'cherry', 'value':4},
-				{'name':'strawberry', 'value':6},
-				{'name':'lemon', 'value':8},
-				{'name':'orange', 'value':10},
-				{'name':'apple', 'value':12},
-				{'name':'pear', 'value':14},
-				{'name':'peach', 'value':16},
-				{'name':'pineapple', 'value':18},
-				{'name':'melon', 'value':20},
-				{'name':'watermelon', 'value':22},
-			]
+			//const fruits = getFruit();
 			// Generate a random index
 			const randomIndex = Math.floor(Math.random() * fruits.length);
 			// Retrieve the random element
@@ -58,8 +77,12 @@ export default class PortentGame extends Phaser.Scene {
 			console.log('randomFruit', randomFruit);
 	
 			let fruit = this.matter.add.image(Phaser.Math.Between(0, 800), 0, randomFruit.name).setIgnoreGravity(true);
+			fruit.setCircle();
+			// @ts-ignore
 			fruit.variety = randomFruit.name;
+			// @ts-ignore
 			fruit.value = randomFruit.value;
+			fruit.body.label = "fruit";
 	
 			return fruit;
 		}
@@ -67,7 +90,7 @@ export default class PortentGame extends Phaser.Scene {
 		/**
 		 * Create a ball at the top center of the screen
 		 */
-		const currentFruit = createFruit();
+		let currentFruit;
 		//activefruits.push(currentFruit);
 		
 		/**
@@ -75,10 +98,40 @@ export default class PortentGame extends Phaser.Scene {
 		 */
 		this.input.on('pointerdown', function (pointer) {
 			console.log('Bucket clicked');
+			let newFruit = createFruit();
+			currentFruit = newFruit;
+			activefruits.push(newFruit);
+
 			currentFruit.x = pointer.x;
 			currentFruit.setIgnoreGravity(false);
-			activefruits.push(createFruit());
 			console.log(activefruits);
+		});
+
+		/**
+		 * Collision event handler
+		 */
+		this.matter.world.on('collisionstart', function (event, bodyA, bodyB) {
+			if(bodyA.label === "fruit" && bodyB.label === "fruit"){
+				console.log(event);
+				console.log("fruitA", bodyA.gameObject.texture.key);
+				console.log("fruitB", bodyB.gameObject.texture.key);
+				if(bodyA.gameObject.texture.key === bodyB.gameObject.texture.key) {
+					console.log(`MATCH ${bodyA.gameObject.texture.key} and ${bodyB.gameObject.texture.key}`);
+					//this.matter.remove(event.world, bodyB);
+					
+					// Lookup our upgraded fruit based on value. 2x the current fruit.
+					const upgradedFruit = fruits.find(item => item.value === (bodyA.gameObject.value*2));
+
+					// Check if the new texture is already loaded
+					if (!this.textures.exists(upgradedFruit.name)) {
+						// If not, dynamically load the new texture
+						this.load.image(upgradedFruit.name);
+					}
+
+					// Set the new texture for the game object
+					bodyA.gameObject.setTexture(upgradedFruit.name);
+				}
+			}
 		});
 		
 	}
